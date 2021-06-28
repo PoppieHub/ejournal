0f2fc3c05a63d6af64b3f49dd88d956c92a32014
Andrey Korolev <hoolm-2012@yandex.ru>
2021-06-28 18:47:13 +0500

    fixed interface bugs

--- src/Repository/DisciplineRepository.php
+++ src/Repository/DisciplineRepository.php
@@ -18,33 +18,4 @@ class DisciplineRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Discipline::class);
     }
-
-    // /**
-    //  * @return Discipline[] Returns an array of Discipline objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('d.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Discipline
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
